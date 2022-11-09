8090da253cf53f3220c2a109b97210a9aafd3cb3
Bruno Navarro <62071446+bnavarroo@users.noreply.github.com>
2022-11-09 20:39:59 -0300

    fix: delete folder

--- utilities/crypto/index.ts
+++
@@ -1,3 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-export const encrypt = (s: string) => CryptoJS.SHA1(CryptoJS.MD5(s)).toString();
--- utilities/hooks/use-guest/types.ts
+++
@@ -1,4 +0,0 @@
-import { IHttpResponse } from '@utilities/http/types';
-import { IGuest } from '@shared/types/guest';
-
-export type TGuestData = IHttpResponse<IGuest> | null;
