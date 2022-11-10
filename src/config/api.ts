89c9fa0b723b02fe5f3f82dbec9bdd1dc4065f87
Bruno Navarro <62071446+bnavarroo@users.noreply.github.com>
2022-11-09 21:16:06 -0300

    update api config

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,7 +3,8 @@ const base = '/api';
 const apiRoutes: { [key: string]: string } = {
   admin: `${base}/admin`,
   guest: `${base}/guest`,
-  guests: `${base}/guests`,
+  guestList: `${base}/guest/list`,
+  user: `${base}/user`,
 };
 
 export const getEndpoint = (endpoint: string, urlParams: Array<string> = []) =>
