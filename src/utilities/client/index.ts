a16138b09e5196298ed6124f4f62b04399f9834f
Bruno Navarro <62071446+bnavarroo@users.noreply.github.com>
2022-11-24 18:25:28 -0300

    fix: utilities/client

--- src/utilities/client/index.ts
+++ src/utilities/client/index.ts
@@ -1,52 +1,5 @@
-import client from '@utilities/client';
-import { encrypt } from '@utilities/crypto';
-import { IUser, IUserModel } from '@shared/types/user';
+import { PrismaClient } from '@prisma/client';
 
-export const createUser = async (
-  name: string,
-  username: string,
-  password: string
-): Promise<IUser | null> => {
-  const encryptedPassword = encrypt(password);
+const client = new PrismaClient();
 
-  const user: IUserModel | null = await client.user.create({
-    data: {
-      name,
-      username,
-      password: encryptedPassword,
-    },
-  });
-
-  if (user) {
-    return {
-      name: user?.name,
-      username: user?.username,
-      isLoggedIn: false,
-    };
-  }
-
-  return null;
-};
-
-export const getUser = async (
-  username: string,
-  password: string
-): Promise<IUser | null> => {
-  const encryptedPassword = encrypt(password);
-  const user: IUserModel | null = await client.user.findFirst({
-    where: {
-      username,
-      password: encryptedPassword,
-    },
-  });
-
-  if (user) {
-    return {
-      name: user?.name,
-      username: user?.username,
-      isLoggedIn: !!user,
-    };
-  }
-
-  return null;
-};
+export default client;
