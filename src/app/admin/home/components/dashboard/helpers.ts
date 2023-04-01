9369a24ce9a47de84dc583a97636973ee942a8fa
Bruno Navarro <62071446+bnavarroo@users.noreply.github.com>
2023-04-01 18:39:40 -0300

    Fix: cálculos convidados não confirmados

--- src/app/admin/home/components/dashboard/helpers.ts
+++ src/app/admin/home/components/dashboard/helpers.ts
@@ -38,7 +38,7 @@ export const getDahsboardData = (data: Array<IGuest>) => {
     totalConfirmedAdults + totalConfirmedChildren / 2
   );
 
-  const totalNotConfirmed = Math.round(totalMainPriority - totalConfirmedAdults - totalConfirmedChildren / 2); // total de não confirmados
+  const totalNotConfirmed = Math.round((totalAdults + totalPayingChildren / 2) - totalConfirmed); // total de não confirmados
 
   return {
     totalAdults,
